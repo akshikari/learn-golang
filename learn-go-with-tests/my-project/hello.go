b3af6668722c148967a035bc93669d5f7b038815
Abhi Kumar <abhinavbkumar@protonmail.com>
2024-02-22 22:04:30 -0600

    switch case, constants, refactor

--- learn-go-with-tests/my-project/hello.go
+++ learn-go-with-tests/my-project/hello.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name + "!"
+
+	return greetingPrefix(language) + name + "!"
 }
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+
+}
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
