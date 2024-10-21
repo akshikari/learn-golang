3510ebb74acaf843ffa8e61c644d228dfb13d6b3
Abhi Kumar <abhinavbkumar@protonmail.com>
2024-10-21 14:37:37 -0500

    reverted Countdown to original fxnality which sleeps b/w each count

--- learn-go-with-tests/mocking/main.go
+++ learn-go-with-tests/mocking/main.go
@@ -45,22 +45,22 @@ const finalWord = "Go!"
 const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-	// for i := countdownStart; i > 0; i-- {
-	// 	fmt.Fprintln(out, i)
-	// 	sleeper.Sleep()
-	// }
-	// fmt.Fprint(out, finalWord)
-
-	for i := countdownStart; i > 0; i-- {
-		sleeper.Sleep()
-	}
-
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		sleeper.Sleep()
 	}
-
-	// sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
+
+	// for i := countdownStart; i > 0; i-- {
+	// 	sleeper.Sleep()
+	// }
+	//
+	// for i := countdownStart; i > 0; i-- {
+	// 	fmt.Fprintln(out, i)
+	// }
+	//
+	// // sleeper.Sleep()
+	// fmt.Fprint(out, finalWord)
 }
 
 func main() {
