e94ded4c164abcc90e0cb84038680675adc3d137
Abhi Kumar <abhinavbkumar@protonmail.com>
2024-10-21 15:07:52 -0500

    Added ConfigurableSleeper, Removed redundant structs

--- learn-go-with-tests/mocking/countdown_test.go
+++ learn-go-with-tests/mocking/countdown_test.go
@@ -4,14 +4,14 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
 	t.Run("prints 3 to Go!", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		spySleeper := &SpySleeper{}
 
-		Countdown(buffer, spySleeper)
+		Countdown(buffer, &SpyCountdownOperations{})
 
 		got := buffer.String()
 		want := `3
@@ -21,10 +21,6 @@ Go!`
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
-		}
-
-		if spySleeper.Calls != 3 {
-			t.Errorf("not enough calls to sleeper want 3 got %d", spySleeper.Calls)
 		}
 	})
 
@@ -47,4 +43,16 @@ Go!`
 		}
 
 	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
