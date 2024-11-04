777b7cf6e6b23ae06e31f5e726a3a6a06658699e
Abhi Kumar <abhinavbkumar@protonmail.com>
2024-11-04 12:25:28 -0600

    property-based tests, using test/quick, roman<->arabic numeral translation

--- learn-go-with-tests/property_based_tests/roman_kata.go
+++ learn-go-with-tests/property_based_tests/roman_kata.go
@@ -1,21 +1,52 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
-func ConvertToRoman(arabic int) string {
+type RomanNumeral struct {
+	Value  uint16
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic uint16) string {
 	var result strings.Builder
 
-	for i := arabic; i > 0; i-- {
-		if i == 5 {
-			result.WriteString("V")
-			break
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
-		if i == 4 {
-			result.WriteString("IV")
-			break
-		}
-		result.WriteString("I")
 	}
 
 	return result.String()
+}
+
+func ConvertToArabic(roman string) uint16 {
+	var arabic uint16 = 0
+
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+
+	return arabic
 }
