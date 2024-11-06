9426e15a028f361d1bc845160d585857bf34230c
Abhi Kumar <abhinavbkumar@protonmail.com>
2024-11-06 12:40:38 -0600

    Starting maths module

---
+++ learn-go-with-tests/quickmaths/clockface/clockface.go
@@ -0,0 +1,26 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+// A point represents a two-dimensional Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	return Point{150, 240}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func secondHandPoint(t time.Time) Point {
+	return Point{}
+}
---
+++ learn-go-with-tests/quickmaths/go.mod
@@ -0,0 +1,3 @@
+module quickmaths
+
+go 1.23.2
