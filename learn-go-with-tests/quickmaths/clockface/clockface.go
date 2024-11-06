8350210a317a67ab624d64599674acf1e27651c5
Abhi Kumar <abhinavbkumar@protonmail.com>
2024-11-06 13:18:13 -0600

    Completed math for calculating second hand position

--- learn-go-with-tests/quickmaths/clockface/clockface.go
+++ learn-go-with-tests/quickmaths/clockface/clockface.go
@@ -11,10 +11,18 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCenterX = 150
+const clockCenterY = 150
+
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
 func SecondHand(t time.Time) Point {
-	return Point{150, 240}
+	p := secondHandPoint(t)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}         // translate
+	return p
 }
 
 func secondsInRadians(t time.Time) float64 {
@@ -22,5 +30,9 @@ func secondsInRadians(t time.Time) float64 {
 }
 
 func secondHandPoint(t time.Time) Point {
-	return Point{}
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
 }
