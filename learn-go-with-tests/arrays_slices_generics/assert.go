1e158358f8201af49db89bb5296932be1973b7a3
Abhi Kumar <abhinavbkumar@protonmail.com>
2024-11-19 16:56:20 -0600

    more generics, more reducing this time filtering an array vs combining elements

--- learn-go-with-tests/arrays_slices_generics/assert.go
+++ learn-go-with-tests/arrays_slices_generics/assert.go
@@ -2,17 +2,17 @@ package arraysandslicesgenerics
 
 import "testing"
 
-func AssertEqual(t *testing.T, got, want interface{}) {
+func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %+v, want%+v", got, want)
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want interface{}) {
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got == want {
-		t.Errorf("didn't want %+v", got)
+		t.Errorf("didn't want %v", got)
 	}
 }
 
