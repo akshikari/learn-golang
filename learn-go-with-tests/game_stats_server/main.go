dd14ee6c5581ff84b95bbd784338ad2d0ebbbd54
Abhi Kumar <abhinavbkumar@protonmail.com>
2025-03-27 09:46:32 -0500

    game stats server start

---
+++ learn-go-with-tests/game_stats_server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// main.go
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	log.Fatal(http.ListenAndServe(":5001", server))
+}
